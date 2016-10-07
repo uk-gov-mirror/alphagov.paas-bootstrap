4b773fb8f5a3962a1f9eb9604721a6c051ab0e9a
Hector Rivas Gandara <keymon@gmail.com>
2016-10-07 13:46:08 +0100

    Verify the CI account_id using sha256
    We want to ensure that this terraform code is only executed in
    a specific account (CI) but we do not want to commit and make public
    the account id number in the repository.
    
    To workaround this issue, we store and compare the sha256 that is
    stored in the repo. Although it could still be broken by force brute,
    it would be  more difficult and tedious, and we don't store the ID in
    clear text.
    
    The implementation of this is as follows:
     * Use the datasource `data "aws_caller_identity current {}` to retrive
       the current account_id [1]
     * Compute the sha256() of this account_id
     * Check if the sha256 is a member of a hash of precomputed sha256 of
       the allowed account IDs. If the ID is not present, terraform will
       fail.
    
    [1] https://www.terraform.io/docs/providers/aws/d/caller_identity.html

--- terraform/boshreleases/variables.tf
+++ terraform/boshreleases/variables.tf
@@ -1,6 +1,11 @@
-variable "account_ids" {
+# SAH256 of the account IDs. To get it, run:
+#
+#   echo -n 1234567890 | sha256sum
+#
+# being 1234567890 the account id
+variable "account_ids_sha256" {
   default = {
-    ci = "1234567890"
+    "19cadd55dbfd1ec914844038f4089e612cbde5621351059a6c86aecbf1407eb9" = "ci"
   }
 }
 
