8d10e4d907c223c00f263fd5f1bd967a015358c3
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2017-01-26 16:17:58 +0000

    Allow Concourse-lite to authenticate with IAM profile

--- vault/terraform/roles.tf
+++ vault/terraform/roles.tf
@@ -18,3 +18,14 @@ resource "aws_iam_role_policy_attachment" "vault_manage_state_s3_buckets" {
   role       = "${aws_iam_role.vault.name}"
   policy_arn = "${aws_iam_policy.vault_manage_state_s3_buckets.arn}"
 }
+
+resource "aws_iam_policy" "get_instance_profile" {
+  name        = "GetInstanceProfile"
+  description = "Retrieves information about the specified instance profile, including the instance profile's path, GUID, ARN, and role."
+  policy      = "${file("${path.module}/policies-json/get_instance_profile.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "vault_get_instance_profile" {
+  role       = "concourse-lite"
+  policy_arn = "${aws_iam_policy.get_instance_profile.arn}"
+}
