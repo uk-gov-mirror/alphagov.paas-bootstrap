3574f3981556abc10fb3a18deb0546504c5a066c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-12-20 14:17:29 +0000

    WIP: add DNS entries for locally tunneled bosh / uaa

--- terraform/bosh/bosh_dns.tf
+++ terraform/bosh/bosh_dns.tf
@@ -6,6 +6,23 @@ resource "aws_route53_record" "bosh" {
   records = ["${lookup(var.microbosh_ips, var.bosh_az)}"]
 }
 
+resource "aws_route53_record" "bosh-local" {
+  zone_id = "${var.system_dns_zone_id}"
+  name    = "bosh-local.${var.system_dns_zone_name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["127.0.0.1"]
+}
+
+resource "aws_route53_record" "bosh-uaa-local" {
+  zone_id = "${var.system_dns_zone_id}"
+  name    = "bosh-uaa-local.${var.system_dns_zone_name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["127.0.0.1"]
+}
+
+
 resource "aws_route53_record" "bosh-external" {
   zone_id = "${var.system_dns_zone_id}"
   name    = "${var.bosh_fqdn_external}"
